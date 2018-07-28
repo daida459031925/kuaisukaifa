afaaa8f0df9e37cf4ab06ea55bda06d6538ecc84
戴达_上官爱 <daida459031925@163.com>
2018-07-28 12:10:05 +0800

    准备学习jdk

--- src/main/java/JDK/util/Formatter.java
+++ src/main/java/JDK/util/Formatter.java
@@ -4,6 +4,10 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+
 public class Formatter {
     private java.util.Formatter formatter = new java.util.Formatter();
 
@@ -11,5 +15,13 @@ public class Formatter {
     @Test
     public void TestFormatter(){
         log.info(formatter.toString());
+        StringBuffer sb = new StringBuffer("代打");
+        File file = new File("C:\\core.dmp");
+        try {
+            formatter = new java.util.Formatter(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        log.info(formatter.out().toString());
     }
 }
