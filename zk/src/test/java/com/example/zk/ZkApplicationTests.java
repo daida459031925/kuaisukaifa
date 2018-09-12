923cce606a5e482fd2e81d4cfa52f742a06df76b
daida459031925 <30424817+daida459031925@users.noreply.github.com>
2018-09-12 14:16:22 +0800

    将子项目提交

---
+++ zk/src/test/java/com/example/zk/ZkApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.zk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZkApplicationTests {
+//	private ZkClient client = new ZkClient(ZOOKEEPER_IP_PORT, 1000, 1000, new SerializableSerializer());
+	@Test
+	public void contextLoads() {
+
+	}
+
+}
